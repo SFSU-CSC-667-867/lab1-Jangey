156bea052d9da856d55e86fd959276121636ad95
Jangey <jangey.lu@gmail.com>
2019-02-06 21:08:30 -0800

    final modified for all files

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,9 @@
+console.log('Hi !');
 setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now 1'));
 process.nextTick(() => console.log('Hello'));
+setImmediate(() => console.log('Now 2'));
+setImmediate(() => console.log('Now 3'));
 
 console.log('world');
+console.log('world2');
